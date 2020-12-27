9f6c40cc154f8a95ad0d5c70954e7179f0ca5a8c
samir patil <samirspatil742099@gmail.com>
2020-12-27 19:04:03 +0530

    Login is also done

--- Account/urls.py
+++ Account/urls.py
@@ -8,5 +8,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('',views.home,name='home'),
     path('register/',views.register,name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='Account/account_login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='Account/account_login.html'), name='logout'),
 
 ]
--- Account/views.py
+++ Account/views.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 
 def home(request):
     return render(request,'Account/account_home.html')
-def register(request):
 
+def register(request):
 
     if request.user.is_authenticated:
         return redirect('home')
@@ -20,7 +20,7 @@ def register(request):
             return redirect('home')
 
     context={
-        'registerForm':register_form
+        'form':register_form
     }
     return render(request,'Account/account_register.html',context)
 
